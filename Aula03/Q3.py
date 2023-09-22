4c011a2f3abf367930edf5b5d3ce6efdbbf0a199
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2023-09-22 19:52:09 +0000

    Resolução dos exercícios 3 e 4

--- Aula03/Q3.py
+++ Aula03/Q3.py
@@ -5,3 +5,12 @@ digitado e verificar se este terceiro valor está dentro do intervalo ou
 fora. Caso esteja fora do intervalo, deverá informar se está na parte
 inferior ou superior do intervalo.
 """
+numero1 = int(input("Início: "))
+numero2 = int(input("Fim: "))
+
+numero3 = int(input("Verificar qual número? "))
+
+if numero3 >= numero1  and numero3 <= numero2:
+    print("Dentro do intervalo")
+else: 
+    print("Fora do intervalo")
