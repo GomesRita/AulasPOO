b62ececa3010cc77614055463aa3b4ae80c551db
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2023-09-22 19:06:51 +0000

    Resolução dos exercícios 1 e 2

---
+++ Aula03/Q3.py
@@ -0,0 +1,7 @@
+"""
+Escreva um programa que leia dois valores que representem o início
+e o fim de um intervalo. O programa deverá ler um terceiro valor
+digitado e verificar se este terceiro valor está dentro do intervalo ou
+fora. Caso esteja fora do intervalo, deverá informar se está na parte
+inferior ou superior do intervalo.
+"""
