4b9daf8d667a1baf0a772dc110061da78661b09e
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2023-10-26 19:58:48 +0000

    Adicionando questões da prova

---
+++ Prova01/Questao1.py
@@ -0,0 +1,12 @@
+"""
+Escreva um programa em Python para auxiliar uma pessoa a realizar uma lista de compras em um
+supermercado. O seu programa deve permitir que o usuário crie uma lista de compras contendo o
+nome do produto, o seu respectivo valor e o status (comprado ou não comprado). O usuário do
+programa deverá poder cadastrar os produtos e valores na lista de compras, bem como poder
+visualizar a lista de compras a qualquer momento que ele desejar. Caso o usuário encontre o
+produto no supermercado e deseje levar o produto o programa deverá marcar o item como
+comprado. A término das compras, o usuário deverá poder visualizar o total e a quantidade de itens
+comprados, bem como a relação de itens que não foram comprados. (25pts)
+a. OBS: Utilize o tipo, ou os tipos, de coleção(ões) que você considerar mais adequado(s)
+para solucionar o problema.
+"""
---
+++ Prova01/Questao2.py
@@ -0,0 +1,14 @@
+"""
+Escreva um programa em Python para registrar, calcular e decidir o resultado de uma final de
+campeonato de futebol, a qual será decidida em dois jogos. O seu programa deve solicitar o nome
+das duas equipes que irão disputar as partidas da final do campeonato, o placar do primeiro jogo e
+o placar do segundo jogo. A vitória de uma partida vale três pontos e um empate vale um ponto.
+Vence a final do campeonato quem fizer mais pontos. Caso cada uma das equipes vença um dos
+jogos ou ocorram dois empates, vence o campeonato quem tiver feito mais gols no somatório dos
+dois jogos. Se o resultado do campeonato puder ser definido a partir do resultado dos dois
+confrontos, o programa deverá informar quem foi a equipe campeã e o placar dos dois jogos. Caso
+ocorra um empate em ambos os quesitos (pontos e número de gols), o programa deverá informar
+que a partida irá para cobrança de pênaltis e solicitar o placar da decisão por pênaltis. Ao final,
+informar o resultado dos dois confrontos, o resultado da cobrança de pênaltis e o nome da equipe
+campeã. (25pts)
+"""
