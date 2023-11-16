13b48a5000b9c340e966451bd8c7fdd760a9b2d3
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2023-11-16 15:25:55 +0000

    Q2 slide 4

--- Slide4/While/ExercicioFixacao1/Q2.py
+++ Slide4/While/ExercicioFixacao1/Q2.py
@@ -5,8 +5,10 @@ devem ser INSATISFEITO; SATISFEITO; NÃO QUERO RESPONDER. O
 algoritmo deverá ainda perguntar quantas usuários irão responder à
 pergunta. Ao final apresentar o percentual de resposta de cada opção.
 """
-
-print (''' 
+qntd = int(input("Quantas pessoas irão responder a esta pesquisa: " ))
+lista = []
+while qntd >= 1:
+    print (''' 
 Pesquisa de satisfação:
        
     1 - Insatisfeito;
@@ -14,8 +16,14 @@ Pesquisa de satisfação:
     3 - Não quero responder;
 
 ''')
+    opniao = int (input("Informe um número conforme sua opinião: "))
+    
+    if(opniao < 1 or opniao > 3):
+       print ("Informe um valor válido!")
+       opniao = int (input("Informe um número conforme sua opinião: "))
+    else:
+        qntd = qntd - 1
+        lista.append(opniao)
+        print("Resposta enviada")
 
-opniao = int (input("Informe um número conforme sua opinião: "))
-
-if(opniao < 1 or opniao > 3):
-    print ("Informe um valor válido!")
+print(lista)
